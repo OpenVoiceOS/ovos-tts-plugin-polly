1fe4a14465c5a19ce3c331afa7119009eaee1834
jarbasai <jarbasai@mailfence.com>
2022-05-22 19:46:51 +0100

    move to OVOS packaging + Dockerfile

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
-PLUGIN_ENTRY_POINT = 'chatterbox_polly_tts = ' \
-                     'chatterbox_tts_plugin_polly:PollyTTS'
+PLUGIN_ENTRY_POINT = 'ovos-tts-plugin-polly = ' \
+                     'ovos_tts_plugin_polly:PollyTTS'
 setup(
-    name='chatterbox_plugin_polly_tts',
-    version='0.0.2',
-    description='polly tts plugin for chatterbox',
-    url='https://github.com/HelloChatterbox/chatterbox-polly-tts-plugin',
+    name='ovos-tts-plugin-polly',
+    version='0.0.1',
+    description='polly tts plugin for OpenVoiceOS mycroft neon chatterbox',
+    url='https://github.com/OpenVoiceOS/ovos-tts-plugin-polly',
     author='JarbasAi',
     author_email='jarbasai@mailfence.com',
     license='Apache-2.0',
-    packages=['chatterbox_tts_plugin_polly'],
-    install_requires=["boto3", "phoneme_guesser",
-                      'ovos-plugin-manager>=0.0.1a13'],
+    packages=['ovos_tts_plugin_polly'],
+    install_requires=["boto3", 'ovos-plugin-manager>=0.0.1a13'],
     zip_safe=True,
     include_package_data=True,
     classifiers=[
@@ -21,18 +20,8 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Text Processing :: Linguistic',
         'License :: OSI Approved :: Apache Software License',
-
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3'
     ],
-    keywords='chatterbox plugin tts',
+    keywords='OpenVoiceOS mycroft neon chatterbox plugin tts',
     entry_points={'mycroft.plugin.tts': PLUGIN_ENTRY_POINT}
 )
