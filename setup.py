ba9078a16f7cff36b1149401d6c5b9d390d619ed
miro <jarbasai@mailfence.com>
2024-09-10 21:53:39 +0100

    cgore:automations

--- setup.py
+++ setup.py
@@ -1,12 +1,41 @@
 #!/usr/bin/env python3
+import os
+
 from setuptools import setup
 
 PLUGIN_ENTRY_POINT = 'ovos-tts-plugin-polly = ovos_tts_plugin_polly:PollyTTS'
 SAMPLE_CONFIGS = 'ovos-tts-plugin-polly.config = ovos_tts_plugin_polly:PollyTTSPluginConfig'
 
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join('ovos_tts_plugin_polly', 'version.py')
+    print(f"ERROR: version file: {version_file}")
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
+
 setup(
     name='ovos-tts-plugin-polly',
-    version='0.0.1',
+    version=get_version(),
     description='polly tts plugin for OpenVoiceOS mycroft neon chatterbox',
     url='https://github.com/OpenVoiceOS/ovos-tts-plugin-polly',
     author='JarbasAi',
